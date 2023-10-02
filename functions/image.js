778d9a03f0218921487cb47aaf36da899f137127
Abel1011 <abelmendez1360@gmail.com>
2023-10-01 20:38:55 -0500

    [01/10/2023 - Se cambia la grabación de imágenes por backblaze]

--- functions/image.js
+++ functions/image.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const fs = require('fs')
+/* const fs = require('fs')
 const path = require('path')
 const config = require('config')
 
@@ -24,7 +24,71 @@ const saveImage = async (binary, name) => {
   } catch (error) {
     throw error
   }
-}
+} */
+
+const B2 = require('backblaze-b2');
+
+const b2 = new B2({
+  applicationKeyId: process.env.B2_KEY_ID,
+  applicationKey: process.env.B2_KEY,
+});
+
+let authorized = false;
+const maxAttempts = 3;  // Número máximo de intentos de autorización
+
+const handleAuthorization = async () => {
+  try {
+    await b2.authorize();
+    authorized = true;
+  } catch (error) {
+    console.error('Error al autorizar:', error);
+    throw new Error('No se pudo autorizar con Backblaze B2');
+  }
+};
+
+const saveImage = async (binary, name) => {
+  // Buffer del archivo
+  const data = Buffer.from(binary, 'binary');
+
+  let attempts = 0;
+  while (attempts < maxAttempts) {
+    try {
+      // Manejar la autorización
+      if (!authorized) {
+        await handleAuthorization();
+      }
+
+      // Obtener la URL y el token para la carga
+      const { data: { authorizationToken,uploadUrl }} = await b2.getUploadUrl({
+        bucketId: process.env.B2_BUCKET_ID,
+      });
+
+      // Subir el archivo
+      await b2.uploadFile({
+        uploadUrl,
+        uploadAuthToken: authorizationToken,
+        fileName: 'images/' + name + '.png',
+        data: data,
+      });
+
+      return 'images/' + name + '.png'; // Nombre del archivo
+    } catch (error) {
+      console.error('Error al subir el archivo:', error);
+
+      // Si el error está relacionado con la autorización, reiniciar la bandera de autorización
+      if (error.code === 'unauthorized' || error.code === 'expired_auth_token') {
+        authorized = false;
+        attempts++;  // Aumentar el contador de intentos
+      } else {
+        // Si el error es por otra razón, lanzar el error
+        throw error;
+      }
+    }
+  }
+
+  // Si se alcanza el número máximo de intentos, lanzar un error
+  throw new Error('Se alcanzó el número máximo de intentos para subir el archivo');
+};
 
 module.exports = {
   saveImage
