7449456e8a22c8bc1fb51683ea35a79243d010f4
Julio Grados <seikres.ggc@gmail.com>
2023-10-06 10:16:37 -0500

    [6/10/23] - format text whatsapp

---
+++ functions/format.js
@@ -0,0 +1,15 @@
+const FormatText = (text) => {
+  const boldRegex = /\*(.*?)\*/g
+  const italicRegex = /\_(.*?)\_/g
+  const striketroughRegex = /\~(.*?)\~/g
+
+  const textBold = text.replace(boldRegex, "<b>$1</b>")
+  const textItalic = textBold.replace(italicRegex, "<i>$1</i>")
+  const textStriketrough = textItalic.replace(striketroughRegex, "<s>$1</s>")
+
+  return textStriketrough
+}
+
+module.exports = {
+  FormatText
+}
