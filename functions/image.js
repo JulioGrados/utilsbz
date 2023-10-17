87b2eda32d6ce0ae0200e68f9b16230e3df1e68f
Abel1011 <abelmendez1360@gmail.com>
2023-10-17 11:00:07 -0500

    [17/10/2023 - Se crea una clase para gestionar la subida de archivos con gestión automática de tokens]

--- functions/image.js
+++ functions/image.js
@@ -25,75 +25,128 @@ const saveImage = async (binary, name) => {
     throw error
   }
 } */
+const axios = require('axios');
 
-const BB2 = require('backblaze-b2');
+const TOKEN_EXPIRATION_TIME = 86400000; // 24 horas en milisegundos
+const API_BASE_URL = 'https://api.backblazeb2.com/b2api/v2/';
+const INITIAL_DELAY = 200; // Tiempo inicial de espera en milisegundos para reintento exponencial
 
-let b2;
-let authorized = false;
-const maxAttempts = 3;  // Número máximo de intentos de autorización
-
-const handleAuthorization = async () => {
-  try {
-    b2 = new BB2({
-      applicationKeyId: process.env.B2_KEY_ID,
-      applicationKey: process.env.B2_KEY,
-    });
-    await b2.authorize();
-    authorized = true;
-  } catch (error) {
-    console.error('Error al autorizar:', error.message);
-    console.error("respuesta de error", error.response.data);
-    console.error(error);
-    throw new Error('No se pudo autorizar con Backblaze B2');
+class BackblazeManager {
+  constructor(accountId, applicationKey) {
+    this.accountId = accountId;
+    this.applicationKey = applicationKey;
+    this.authData = null;
+    this.authTime = null;
+    this.uploadUrlInfo = null;
+    this.uploadUrlExpireTime = null;
   }
-};
 
-const saveImage = async (binary, name) => {
-  // Buffer del archivo
-  const data = Buffer.from(binary, 'binary');
+  isTokenExpired(lastAuthTime) {
+    return Date.now() - lastAuthTime >= TOKEN_EXPIRATION_TIME;
+  }
 
-  let attempts = 0;
-  while (attempts < maxAttempts) {
+  buildAuthHeader() {
+    return {
+      Authorization: this.authData.authorizationToken,
+    };
+  }
+
+  buildUploadHeaders(uploadInfo, fileName) {
+    return {
+      Authorization: uploadInfo.authorizationToken,
+      'X-Bz-File-Name': fileName,
+      'Content-Type': 'b2/x-auto',
+      'X-Bz-Content-Sha1': 'do_not_verify',
+    };
+  }
+
+  async authorizeIfNeeded() {
+    if (this.authData && !this.isTokenExpired(this.authTime)) {
+      return;
+    }
+
     try {
-      // Manejar la autorización
-      if (!authorized) {
-        await handleAuthorization();
-      }
-
-      // Obtener la URL y el token para la carga
-      const { data: { authorizationToken,uploadUrl }} = await b2.getUploadUrl({
-        bucketId: process.env.B2_BUCKET_ID,
+      const response = await axios({
+        method: 'GET',
+        url: `${API_BASE_URL}b2_authorize_account`,
+        auth: {
+          username: this.accountId,
+          password: this.applicationKey,
+        },
       });
 
-      // Subir el archivo
-      await b2.uploadFile({
-        uploadUrl,
-        uploadAuthToken: authorizationToken,
-        fileName: 'images/' + name + '.png',
-        data: data,
-      });
-
-      return '/images/' + name + '.png'; // Nombre del archivo
+      this.authData = response.data;
+      this.authTime = Date.now();
     } catch (error) {
-      console.error('Error al subir el archivo:', error.message);
-      console.error("respuesta de error", error.response.data);
-      console.error(error);
-
-      // Si el error está relacionado con la autorización, reiniciar la bandera de autorización
-      if (error.code === 'unauthorized' || error.code === 'expired_auth_token' || error.status === 401) {
-        authorized = false;
-        attempts++;  // Aumentar el contador de intentos
-      } else {
-        // Si el error es por otra razón, lanzar el error
-        throw error;
+      if (error.response && error.response.status !== 401) {
+        throw new Error('Error al autorizar la cuenta');
       }
     }
   }
 
-  // Si se alcanza el número máximo de intentos, lanzar un error
-  throw new Error('Se alcanzó el número máximo de intentos para subir el archivo');
+  async getUploadUrlIfNeeded(bucketId) {
+    if (this.uploadUrlInfo && !this.isTokenExpired(this.uploadUrlExpireTime)) {
+      return this.uploadUrlInfo;
+    }
+
+    await this.authorizeIfNeeded();
+
+    const response = await axios.get(`${this.authData.apiUrl}/b2api/v2/b2_get_upload_url`, {
+      params: { bucketId },
+      headers: this.buildAuthHeader(),
+    });
+
+    this.uploadUrlInfo = response.data;
+    this.uploadUrlExpireTime = Date.now();
+    return this.uploadUrlInfo;
+  }
+
+  async uploadFile(bucketId, fileName, data, retries = 3) {
+    let delay = INITIAL_DELAY;
+
+    for (let i = 0; i < retries; i++) {
+      try {
+        const uploadInfo = await this.getUploadUrlIfNeeded(bucketId);
+        const response = await axios.post(uploadInfo.uploadUrl, data, {
+          headers: this.buildUploadHeaders(uploadInfo, fileName),
+        });
+        return response.data;
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          await this.authorizeIfNeeded();
+        } else {
+          console.log(`Error al subir el archivo, reintentando... (${i + 1}/${retries})`);
+          await new Promise(resolve => setTimeout(resolve, delay));
+          delay *= 2; // Exponencial backoff
+        }
+      }
+    }
+
+    throw new Error('Error al subir el archivo después de varios intentos');
+  }
+}
+
+// Crea una nueva instancia de BackblazeManager
+const backblazeManager = new BackblazeManager('f1c0d42c6433', '0053ea1af114767812dea13afc04afe18c224d3dda');
+
+const saveImage = async (binary, name) => {
+  // Buffer del archivo
+  const data = Buffer.from(binary, 'binary');
+  const fileName = 'images/' + name + '.png';
+  try {
+    const bucketId = 'ff317c409d44e2dc86a40313';
+    const uploadResponse = await backblazeManager.uploadFile(bucketId, fileName, data);
+    console.log('Archivo subido con éxito:', uploadResponse);
+    return "/"+fileName;
+  } catch (error) {
+    console.error('Error al guardar la imagen:', error);
+    throw error;
+  }
 };
+
+
 
 module.exports = {
   saveImage
 }
+
