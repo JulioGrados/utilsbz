39ec8916e794044cb874d4548742e5def782fdac
Julio Grados <seikres.ggc@gmail.com>
2024-02-13 19:44:27 -0500

    [13/2/23] - links messages chats

--- functions/format.js
+++ functions/format.js
@@ -2,8 +2,17 @@ const FormatText = (text) => {
   const boldRegex = /\*(.*?)\*/g
   const italicRegex = /\_(.*?)\_/g
   const striketroughRegex = /\~(.*?)\~/g
-
-  const textBold = text.replace(boldRegex, "<b>$1</b>")
+  const matches = text.match(/\bhttps?:\/\/\S+/gi)
+  
+  let text2
+  if (matches && matches.length) {
+    matches.map((item) => {
+      text2 = text.replace(item, `<a href=${item} target='_blank'>${item}</a>`)
+    })
+  } else {
+    text2 = text
+  }
+  const textBold = text2.replace(boldRegex, "<b>$1</b>")
   const textItalic = textBold.replace(italicRegex, "<i>$1</i>")
   const textStriketrough = textItalic.replace(striketroughRegex, "<s>$1</s>")
   
