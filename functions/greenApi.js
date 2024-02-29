be3b5c0df5ba463ac76699abfccd19c43aea4fd6
Julio Grados <seikres.ggc@gmail.com>
2024-02-29 16:49:23 -0500

    [29/2/23] - eliminar mensaje y reenviar

--- functions/greenApi.js
+++ functions/greenApi.js
@@ -20,6 +20,50 @@ const deleteMessageGreen = async (idInstance, token, chatId, idMessage) => {
   }
 }
 
+const sendMessageTextGreen = async (idInstance, token, chatId, text) => {
+  try {
+    const resp = await axios({
+      method: 'POST',
+      url: `https://api.greenapi.com/waInstance${idInstance}/sendMessage/${token}`,
+      data: {
+        chatId: `${chatId}@c.us`,
+        message: text
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    console.log('resp', resp)
+    return resp
+  } catch (error) {
+    throw error
+  }
+}
+
+const sendMessageMediaGreen = async (idInstance, token, chatId, url, filename = '', caption = '') => {
+  try {
+    const resp = await axios({
+      method: 'POST',
+      url: `https://api.greenapi.com/waInstance${idInstance}/sendFileByUrl/${token}`,
+      data: {
+        chatId: `${chatId}@c.us`,
+        urlFile: url,
+        fileName: filename,
+        caption: caption
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    console.log('resp', resp)
+    return resp
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
-  deleteMessageGreen
+  deleteMessageGreen,
+  sendMessageTextGreen,
+  sendMessageMediaGreen
 }
