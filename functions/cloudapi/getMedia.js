d215f05086c35cb91dde659ec85a5483957bcbec
Julio Giampiere Grados Caballero <juliogiampieregradoscaballero@Mac-mini-de-Julio.local>
2024-05-02 22:09:41 -0500

    [2/5/24] - refactorización envio mensaje

--- functions/cloudapi/getMedia.js
+++ functions/cloudapi/getMedia.js
@@ -32,6 +32,117 @@ const getMedia = async (id, token) => {
   }
 }
 
+const setMessage = async (id, token, chat, message) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: `https://graph.facebook.com/v17.0/${id}/messages`,
+      data: {
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to: chat.mobile,
+        type: 'text',
+        text: {
+          body: message
+        }
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    return response
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+const setImage = async (id, token, chat, message, file) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: `https://graph.facebook.com/v17.0/${id}/messages`,
+      data: {
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to: chat.mobileCode + chat.mobile,
+        type: 'image',
+        image: {
+          link: `https://bizeus-test.s3.us-east-2.amazonaws.com${file}`,
+          caption: message
+        }
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    return response
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+const setVideo = async (id, token, chat, message, file) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: `https://graph.facebook.com/v17.0/${id}/messages`,
+      data: {
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to: chat.mobileCode + chat.mobile,
+        type: 'video',
+        video: {
+          link: `https://bizeus-test.s3.us-east-2.amazonaws.com${file}`,
+          caption: message
+        }
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    return response
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+const setDocument = async (id, token, chat, message, file) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: `https://graph.facebook.com/v17.0/${id}/messages`,
+      data: {
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to: chat.mobileCode + chat.mobile,
+        type: 'document',
+        document: {
+          link: `https://bizeus-test.s3.us-east-2.amazonaws.com${file}`,
+          caption: message
+        }
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    return response
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
 module.exports = {
-  getMedia
+  getMedia,
+  setMessage,
+  setImage,
+  setVideo,
+  setDocument
 }
