bdd7921dcc6b4754c701df040809b369502ab61c
Abel1011 <45209123+Abel1011@users.noreply.github.com>
2024-10-02 15:28:22 -0500

    [2/10/2024 Se cambia función de formateo de texto]
    [2/10/2024 Se cambia función de formateo de texto]

--- functions/format.js
+++ functions/format.js
@@ -1,23 +1,36 @@
 const FormatText = (text) => {
-  const boldRegex = /\*(.*?)\*/g
-  const italicRegex = /\_(.*?)\_/g
-  const striketroughRegex = /\~(.*?)\~/g
-  const matches = text.match(/\bhttps?:\/\/\S+/gi)
+  const boldRegex = /\*(.*?)\*/g;
+  const italicRegex = /\_(.*?)\_/g;
+  const striketroughRegex = /\~(.*?)\~/g;
   
-  let text2
-  if (matches && matches.length) {
-    matches.map((item) => {
-      text2 = text.replace(item, `<a href=${item} target='_blank'>${item}</a>`)
-    })
-  } else {
-    text2 = text
+  const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+  
+  const urlRegex = /\b((https?:\/\/|www\.)\S+)\b/gi;
+
+  //formateo de negrita, cursiva y tachado
+  let formattedText = text.replace(boldRegex, "<b>$1</b>")
+                          .replace(italicRegex, "<i>$1</i>")
+                          .replace(striketroughRegex, "<s>$1</s>");
+
+  // Reemplazo de URLs
+  const urlMatches = formattedText.match(urlRegex);
+  if (urlMatches && urlMatches.length) {
+    urlMatches.forEach((item) => {
+      const hrefValue = item.startsWith('www.') ? `http://${item}` : item;
+      formattedText = formattedText.replace(item, `<a href="${hrefValue}" target="_blank">${item}</a>`);
+    });
   }
-  const textBold = text2.replace(boldRegex, "<b>$1</b>")
-  const textItalic = textBold.replace(italicRegex, "<i>$1</i>")
-  const textStriketrough = textItalic.replace(striketroughRegex, "<s>$1</s>")
-  
-  return textStriketrough
-}
+
+  // Reemplazo de correos electrónicos
+  const emailMatches = formattedText.match(emailRegex);
+  if (emailMatches && emailMatches.length) {
+    emailMatches.forEach((item) => {
+      formattedText = formattedText.replace(item, `<a href="mailto:${item}">${item}</a>`);
+    });
+  }
+
+  return formattedText;
+};
 
 module.exports = {
   FormatText
