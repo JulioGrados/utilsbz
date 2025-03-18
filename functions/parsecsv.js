350fc4cc00cb1f0cbb292c4928f67c59dbd74590
Julio Giampiere Grados Caballero <juliogiampieregradoscaballero@Mac-mini-de-Julio.local>
2025-03-18 17:13:02 -0500

    [18/3/25] - utf 8

--- functions/parsecsv.js
+++ functions/parsecsv.js
@@ -6,7 +6,8 @@ const { Readable } = require("stream");
 const parseCSV = async (buffer) => {
     return new Promise((resolve, reject) => {
         const results = [];
-        const stream = Readable.from(buffer.toString());
+        const decodedString = iconv.decode(buffer, "utf8"); // Si sigue fallando, prueba con "latin1"
+        const stream = Readable.from(decodedString);
 
         stream
             .pipe(csvParser()) // Usa la primera fila como nombres de las claves
