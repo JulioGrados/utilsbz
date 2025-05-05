684f647b15e75dc6944d79393fc6712374e97f23
Julio Giampiere Grados Caballero <juliogiampieregradoscaballero@Mac-mini-de-Julio.local>
2025-05-04 19:23:20 -0500

    [4/5/24] - export csv xlsx file

--- functions/csv.js
+++ functions/csv.js
@@ -1,12 +1,31 @@
-const { saveAs } = require("file-saver");
 const Papa = require("papaparse");
+const XLSX = require("xlsx");
+const { saveAs } = require("file-saver");
 
-export const exportToCSV = (data, filename = "data.csv") => {
+const exportToCSV = (data, filename = "data.csv") => {
     const csv = Papa.unparse(data);
     const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
     saveAs(blob, filename);
 };
 
+const exportToXLSX = (data, filename = "data.xlsx") => {
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Datos");
+
+    const excelBuffer = XLSX.write(workbook, {
+        bookType: "xlsx",
+        type: "array",
+    });
+
+    const blob = new Blob([excelBuffer], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+
+    saveAs(blob, filename);
+};
+
 module.exports = {
-    exportToCSV
+    exportToCSV,
+    exportToXLSX,
 };
